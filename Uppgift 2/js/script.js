17254b9bc6df075223b43c0368429701f079b6de
simon-soderberg <simon.sod92@gmail.com>
2021-01-27 14:03:54 +0100

    uppdaterat uppgift 2

--- Uppgift 2/js/script.js
+++ Uppgift 2/js/script.js
@@ -1,19 +1,30 @@
+
+// deklararar för add knappen och för form
+const add = document.querySelector('#add');
+const addIcon = document.querySelector('#addicon');
+const addIcon2 = document.querySelector('#addicon2');
+const formHolder = document.querySelector('#form-holder')
 const form = document.querySelector('#form');
-const input = document.querySelector('#todoInput');
+const input = document.querySelector('#input');
+const addTodobtn = document.querySelector('#addTodo')
 const output = document.querySelector('#output')
 const errorMessage = document.querySelector('#error-message');
-const done = document.querySelector('.check')
 
 
+// skapar arrayen
+let todos = [];
 
-let todos = []; 
 
+// den här ändrar add knappen
+let addvalue = true;
+
+// Hämtar hem alla todos från jasonplaceholders
 const fetchTodos = () => {
   fetch('https://jsonplaceholder.typicode.com/todos?_start=0&_limit=10')
   .then(response => response.json())
   .then(data => {
     todos = data;
-    console.log(todos)
+    // console.log(todos)
     listTodos();
     // console.log(data)
   })
@@ -23,130 +34,113 @@ fetchTodos();
 
 
 
-
-
-
-
-
-
-
-
-
-
-
 const newTodo = (todo) => {
   let card = document.createElement('div');
-  card.classList.add('card', 'p-3')
+  card.classList.add('card', 'py-3', 'my-3', 'animate')
   card.setAttribute('id', `${todo.id}`)
-
-  if(todo.completed) {
-    card.classList.add('bg-green')
-  }
-  else {
-    card.classList.remove('bg-green')
-  }
-
 
   let innerCard = document.createElement('div');
   innerCard.classList.add('d-flex', 'justify-content-between', 'align-items-center');
 
   let innerInnerCard = document.createElement('div');
-  innerInnerCard.classList.add('d-flex', 'align-items-center');
+  innerInnerCard.classList.add('d-flex', 'align-items-center')
 
-  let innerCardInput = document.createElement('input');
-  innerCardInput.classList.add('check');
-  innerCardInput.setAttribute('type', 'checkbox');
-
-
+  let h5 = document.createElement('h5');
+  h5.classList.add('title', 'mx-3')
+  h5.innerText = todo.title
 
   if(todo.completed) {
-    innerCardInput.checked = true
-    
+    h5.classList.add('text-decoration')
   }
 
+  let innerInnerCardButtons = document.createElement('div');
+  
+  let buttonRemove = document.createElement('button');
+  buttonRemove.classList.add('button', 'bg-removed', 'd-none')
+  
+  buttonRemove.addEventListener('click', (e) => {
+    todos = todos.filter(todo => todo.id != e.target.parentNode.parentNode.parentNode.id)
+    listTodos();
 
-  innerCardInput.addEventListener('click', () => {
-    card.classList.toggle('bg-green')
-    button.classList.toggle('done')
-
-
-    todos.map(g => {
-      if(g.id === todo.id) {
-        todo.completed = !todo.completed
-        console.log(g)
-      }
-      return g
-      
-    })
+    // console.log(todos)
   })
 
 
-  let h3 = document.createElement('h3');
-  h3.classList.add('title', 'mx-3')
-  h3.innerText = todo.title
 
-
-  let button = document.createElement('button');
-  button.classList.add('btn', 'btn-danger')
-  button.innerText = 'X';
+  let buttonCheck = document.createElement('button');
+  buttonCheck.classList.add('button')
 
   if(todo.completed) {
-    button.classList.add('done')
-  }
-  else {
-    button.classList.remove('done')
+    buttonCheck.classList.add('bg-checked')
+    buttonRemove.classList.remove('d-none')
   }
 
 
-  innerInnerCard.appendChild(innerCardInput)
-  innerInnerCard.appendChild(h3)
-  innerCard.appendChild(innerInnerCard)
-  innerCard.appendChild(button)
+  // Kör min event lissener för button check här
+  buttonCheck.addEventListener('click', () => {
+    buttonCheck.classList.toggle('bg-checked')
+    buttonRemove.classList.toggle('d-none')
+    h5.classList.toggle('text-decoration')
+
+    todo.completed = !todo.completed;
+
+  })
+
  
+  innerInnerCardButtons.appendChild(buttonRemove);
+  innerInnerCardButtons.appendChild(buttonCheck);
+
+  innerInnerCard.appendChild(h5);
+
+  innerCard.appendChild(innerInnerCard);
+  innerCard.appendChild(innerInnerCardButtons);
+  
+
   card.appendChild(innerCard)
-  output.appendChild(card)
-
-
+  output.insertBefore(card, document.querySelector('#output .card'));
 }
 
-const listTodos = () => {
-  
-  output.innerHTML = '';
 
+
+
+
+
+const listTodos = () => {
+  sortArray()
+  output.innerHTML = '';
   todos.forEach(todo => {
     newTodo(todo)
   })
-
 }
 
 
+const createTodo = async (title) => {
+  try {
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+      },
+      body:JSON.stringify({
+        title,
+        completed: false
+      })
+    });
+    const data = await res.json();
+    todos.unshift(data);
+    newTodo(data)
+  } 
+  catch(error) {
+    console.log(error);
+  }
 
-const createTodo = (title) => {
-
-  fetch('https://jsonplaceholder.typicode.com/todos', {
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8'
-    },
-    body: JSON.stringify({
-      title,
-      completed: false
-    })
-    
-  })
-  .then(res => res.json())
-  .then(data => {
-    console.log(data)
-    todos.unshift(data)
-    listTodos();
-
-  })
 }
+
 
 
 
 const validate = () => {
-  if(input.value == '') {
+  if (input.value == '') {
     input.classList.add('is-invalid')
     errorMessage.innerText = 'Du måste skrive en Todo!!'
     return false
@@ -155,52 +149,54 @@ const validate = () => {
     input.classList.remove('is-invalid');
     errorMessage.innerText = ''
     return true
+
   }
 }
 
 
-
-form.addEventListener('submit', e => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
-  if (validate()) {
-    createTodo(input.value);
-    console.log(input.value)
-    input.value = '';
 
+  if(validate()) {
+    createTodo(input.value);
+    // console.log(input.value)
+    input.value = '';
+    formHolder.classList.add('d-none');    
+    addIcon.classList.remove('d-none');
+    addIcon2.classList.add('d-none');
+
+    addvalue = true
+  }
+
+})
+
+
+
+
+
+
+// Denna funktionen tar fram formuläret
+add.addEventListener('click', () => {
+  if (addvalue == true) {
+    addIcon.classList.add('d-none');
+    addIcon2.classList.remove('d-none');
+
+    formHolder.classList.remove('d-none')
+    addvalue = false;
+    input.focus();
+  }
+  else if (addvalue == false) {
+    addIcon.classList.remove('d-none');
+    addIcon2.classList.add('d-none');
+
+    formHolder.classList.add('d-none')
+
+    addvalue = true;
   }
 })
 
 
 
-
-
-
-
-
-output.addEventListener('click', (e) => {
-  // console.log(e.target.parentNode.parentNode.id)
-
-  if(e.target.classList.contains('done')) {
-    todos = todos.filter(todo => todo.id != e.target.parentNode.parentNode.id)
-      // console.log(todo.id)
-      // console.log('detta fungerar inte')
-      // console.log(todos)
-
-    
-    
-
-    // console.log(todo.id)
-    // console.log(users)
-    // console.log(todos)
-    listTodos();
+function sortArray() {
+  todos.sort((a,b) => b.completed - a.completed)
 }
-else {
-  // console.log('nått är inte rätt')
-}
-
-
-})
-
-
-
-
